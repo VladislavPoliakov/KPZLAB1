99a0332f0864f936846e5f9035bd80d669ffadb4
VladislavPoliakov <poliakov.vladislav.v@gmail.com>
2023-12-11 23:38:02 +0200

    Added a few more examples to ArrayList "library"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,6 +19,8 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Book> library = new ArrayList<Book>();
         library.add(new Book("Head first. Java", "Kathy Sierra", 9785699545742, 2003));
-
+        library.add(new Book("Effective Java", "Joshua Bloch", 9780132345286, 2008));
+        library.add(new Book("Java Concurency in Practice", "Brian Goetz", 9780321349606, 2006));
+        library.add(new Book("java 8 Pocket guide", "Robert Liguory", 9785845920508, 2015));
     }
 }
