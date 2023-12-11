86337f50bd00d77de1f7e1b43780861dc7c6366f
VladislavPoliakov <poliakov.vladislav.v@gmail.com>
2023-12-11 23:17:42 +0200

    Created Book class to store information about books in object, added ArrayList that will store them, and first object of this class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
 
+class Book {
+    public String name;
+    public String author;
+    public int isbn;
+    public int year;
+
+    public Book(String nm, String auth, int num, int yr){
+        this.name = nm;
+        this.author = auth;
+        this.isbn = num;
+        this.year = yr;
+    }
+}
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Book> library = new ArrayList<Book>();
+        library.add(new Book("Head first. Java", "Kathy Sierra", 9785699545742, 2003));
 
     }
 }
