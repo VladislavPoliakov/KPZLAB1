f16656b701eb6d49f1ff0ff1bbf780c075832a9b
VladislavPoliakov <poliakov.vladislav.v@gmail.com>
2023-12-14 14:38:39 +0200

    Debug & added in/output of 1 menu point and default(wrong input). Added point "9"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Book {
     public String name;
@@ -17,33 +18,69 @@ class Book {
     }
 }
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArrayList<Book> library = new ArrayList<Book>();
         library.add(new Book("Head first. Java", "Kathy Sierra", "9785699545742", 2003));
         library.add(new Book("Effective Java", "Joshua Bloch", "9780132345286", 2008));
         library.add(new Book("Java Concurency in Practice", "Brian Goetz", "9780321349606", 2006));
         library.add(new Book("java 8 Pocket guide", "Robert Liguory", "9785845920508", 2015));
         //menu
-        bool menuExt;
-        String readString;
+        boolean menuExt = false;
         int readInt;
+        String readString;
+        Scanner console = new Scanner(System.in);
         while(!menuExt){
             System.out.println("MENU. ENTER NUMBER TO MAKE:");
             System.out.println("1.Add book");
             System.out.println("2.Show all books");
             System.out.println("3.Search by name");
             System.out.println("4.Delete by ISBN");
-            try {
-                readInt = System.in.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            System.out.println("9.EXIT");
+
+                readInt = console.nextInt();
+                readString = console.nextLine();
             switch (readInt){
-                case 1: break;
-                case 2: break;
-                case 3: break;
-                case 4: break;
-                default: break;
+                case 1: {
+                    String nameTemp;
+                    String authorTemp;
+                    String isbnTemp;
+                    int yearTemp;
+                    System.out.println("Enter name:");
+                    readString = console.nextLine();
+                    nameTemp = readString;
+                    System.out.println("Enter author:");
+                    readString = console.nextLine();
+                    authorTemp = readString;
+                    System.out.println("Enter ISBN:");
+                    readString = console.nextLine();
+                    isbnTemp = readString;
+                    System.out.println("Enter year:");
+                    readInt = console.nextInt();
+
+                    yearTemp = readInt;
+                    break;
+
+                }
+                case 2: {
+
+                    break;
+                }
+                case 3: {
+
+                    break;
+                }
+                case 4: {
+
+                    break;
+                }
+                case 9: {
+
+                    break;
+                }
+                default: {
+                    System.out.println("ERROR! INCORRECT INPUT");
+                    break;
+                }
             }
 
         }
