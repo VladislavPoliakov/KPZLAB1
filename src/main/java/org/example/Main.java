57f4bd6b1be400eacb21ffa6f62f1b5be1ec3068
VladislavPoliakov <poliakov.vladislav.v@gmail.com>
2023-12-14 13:53:12 +0200

    Added more examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 class Book {
     public String name;
     public String author;
-    public int isbn;
+    public String isbn;
     public int year;
 
-    public Book(String nm, String auth, int num, int yr){
+    public Book(String nm, String auth, String num, int yr){
         this.name = nm;
         this.author = auth;
         this.isbn = num;
@@ -18,9 +18,9 @@ class Book {
 public class Main {
     public static void main(String[] args) {
         ArrayList<Book> library = new ArrayList<Book>();
-        library.add(new Book("Head first. Java", "Kathy Sierra", 9785699545742, 2003));
-        library.add(new Book("Effective Java", "Joshua Bloch", 9780132345286, 2008));
-        library.add(new Book("Java Concurency in Practice", "Brian Goetz", 9780321349606, 2006));
-        library.add(new Book("java 8 Pocket guide", "Robert Liguory", 9785845920508, 2015));
+        library.add(new Book("Head first. Java", "Kathy Sierra", "9785699545742", 2003));
+        library.add(new Book("Effective Java", "Joshua Bloch", "9780132345286", 2008));
+        library.add(new Book("Java Concurency in Practice", "Brian Goetz", "9780321349606", 2006));
+        library.add(new Book("java 8 Pocket guide", "Robert Liguory", "9785845920508", 2015));
     }
 }
